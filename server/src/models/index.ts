f2198a85ab9a34e403f771d5bd95330811bffde1
hanumanthreddyg <hanugopavarapu@gmail.com>
2025-08-10 09:27:45 -0700

    Fix MongoDB environment variable name from MONGO_URI to MONGODB_URI

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const DBconnect = async () => {
     try {
 
-        await mongoose.connect(process.env.MONGO_URI as string);
+        await mongoose.connect(process.env.MONGODB_URI as string);
         console.log("DB connected")
     } catch (e) {
         console.log("not connected to DB");
